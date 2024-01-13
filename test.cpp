278670a3b142a0caa78d111c5d53dd2a088d5202
ZhaoHanHong <145885662+ZhaoHanHong@users.noreply.github.com>
2024-01-13 23:29:24 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,31 @@
+// 编译环境：Visual C++ 6.0~2022，EasyX_2023大暑版
+// https://easyx.cn
+//
+#include <graphics.h>
+/*
+int main()
+{
+	// 初始化图形窗口
+	initgraph(640, 480);
+
+	ExMessage m;		// 定义消息变量
+
+	while (true)
+	{
+		// 获取一条鼠标或按键消息
+		m = getmessage(EX_MOUSE | EX_KEY);
+
+		switch (m.message)
+		{
+		case WM_LBUTTONDOWN:
+			// 鼠标移动的时候画红色的小点
+			putpixel(m.x, m.y, RED);
+			break;
+		}
+	}
+
+	// 关闭图形窗口
+	closegraph();
+	return 0;
+}
+*/
